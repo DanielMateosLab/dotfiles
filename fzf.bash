218074662c9c212c49312e3dd7a05064d1e5d938
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-02-18 21:01:34 +0100

    Add ubuntu fzf sourcing

--- fzf.bash
+++ fzf.bash
@@ -1,13 +1,25 @@
+## Arch / Manjaro ##
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/dani/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/home/dani/.fzf/bin"
 fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/home/dani/.fzf/shell/completion.bash" 2> /dev/null
-
 # Key bindings
 # ------------
-source "/home/dani/.fzf/shell/key-bindings.bash"
+# source "/home/dani/.fzf/shell/key-bindings.bash"
+if [ -f "/home/dani/.fzf/shell/key-bindings.bash" ]; then
+  source "/home/dani/.fzf/shell/key-bindings.bash"
+fi
+
+## Ubuntu ##
+# source /usr/share/doc/fzf/examples/key-bindings.bash
+if [ -f /usr/share/doc/fzf/examples/key-bindings.bash ]; then
+  source /usr/share/doc/fzf/examples/key-bindings.bash
+fi
+# source /usr/share/doc/fzf/examples/completion.bash
+if [ -f /usr/share/doc/fzf/examples/completion.bash ]; then
+  source /usr/share/doc/fzf/examples/completion.bash
+fi
